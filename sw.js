01b4fb47668d51161365256a2932bed0303dd42c
linguaflip5959 <dhillon52@sontunca.com>
2026-08-13 13:41:45 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,68 @@
+const CACHE_NAME = '3d-calc-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icon.svg'
+];
+
+// Установка и кэширование
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+  self.skipWaiting();
+});
+
+// Перехват запросов (Cache-First)
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Если ресурс есть в кэше - отдаем его
+        if (response) {
+          return response;
+        }
+        // Иначе идем в сеть
+        return fetch(event.request).then(
+          response => {
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+            // Кэшируем новый ресурс
+            let responseToCache = response.clone();
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, responseToCache);
+              });
+            return response;
+          }
+        ).catch(() => {
+          // Офлайн фоллбек (если нужно)
+        });
+      })
+  );
+});
+
+// Очистка старого кэша
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
